bd57eda8442ea62ba1efd47938babdf2f69e2fe2
skucheria <Kucheria-Mac@Siddharths-MacBook-Pro.local>
2015-07-20 00:40:44 -0400

    added a profile view controller. going to put a UIImageView for profile pic.

---
+++ Project/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Project
+//
+//  Created by Siddharth Kucheria on 7/20/15.
+//  Copyright (c) 2015 Siddharth Kucheria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
---
+++ Project/ProfileViewController.m
@@ -0,0 +1,53 @@
+//
+//  ProfileViewController.m
+//  Project
+//
+//  Created by Siddharth Kucheria on 7/20/15.
+//  Copyright (c) 2015 Siddharth Kucheria. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+#import <Parse/Parse.h>
+
+@interface ProfileViewController ()
+@property UILabel *name;
+
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor =[UIColor colorWithRed:0.38 green:0.58 blue:0.92 alpha:1.0];
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+
+    PFUser *current = [PFUser currentUser];
+    UILabel *nameLabel = [[UILabel alloc]initWithFrame:CGRectMake(15, 20, 100, 200)];
+    nameLabel.text = current.username;
+
+    nameLabel.textColor = [UIColor whiteColor];
+    [self.view addSubview:nameLabel];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
